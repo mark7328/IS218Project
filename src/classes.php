096ca7b73ded548b5c8f40a994d0b7851cb04899
Martin Chukwu <mark7328chukwu@gmail.com>
2019-03-17 09:35:23 -0400

    Classes class is used so I only have to include one class (this one) to access all others

---
+++ src/classes.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mark7
+ * Date: 3/16/2019
+ * Time: 9:33 AM
+ */
+include "arrayFunctions.php";
+include "htmlTags.php";
+include "main.php";
+include "record.php";
+include "csvFunctions.php";
+include "csv.php";
+include "factory.php";
+class classes
+{
+
+}
